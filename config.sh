e1e960c23c18316e3ea2df656bdc560f0236e22e
VIGNESH K <107059364+vigneshvicky2@users.noreply.github.com>
2023-09-11 12:32:15 +0530

    twrp

--- config.sh
+++ config.sh
@@ -6,10 +6,10 @@ export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp
 export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="yogurt" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_micromax_yogurt/tree/full_k69v1_64-user-10-QP1A.190711.020-1620836268-release-keys" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="micromax" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
